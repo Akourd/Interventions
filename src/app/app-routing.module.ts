e28b3b8aa17b6e0c76179c811191862f93e9fabf
Amir Kourd <amir_kourd@hotmail.com>
2021-03-20 20:15:59 -0400

    Travail 5-6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'accueil', component:AccueilComponent},
+      { path:'', redirectTo:'accueil', pathMatch:'full'},
+      
+  { path:'probleme', component:ProblemeComponent},
+      { path:'', redirectTo:'probleme', pathMatch:'full'},
+  { path:'**', redirectTo:'accueil', pathMatch:'full'} //si la route est inexistante rediriger l'utilisateur sur accueil
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { RouterModule } from '@angular/router';
+import { PrenomComponent } from './prenom/prenom.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProblemeComponent,
+    PrenomComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
